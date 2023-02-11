b72c8c12cb6e91724785b13d983c3ff9441ae163
Jack Lach <jack.lach4@gmail.com>
2023-02-11 03:38:55 -0500

    fetchTotalBookPages() working

--- index.js
+++ index.js
@@ -14,6 +14,47 @@ function renderBooks(books) {
   });
 }
 
+function fetchTotalBookPages() {
+  fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => renderTotalPages(json));
+}
+
+function renderTotalPages(books) {
+  let pageCount = 0;
+  books.forEach(book => {
+    pageCount += book.numberOfPages;
+  });
+  const h3 = document.createElement('h3');
+  h3.textContent = `The total page count of the books in the series is: ${pageCount}`
+  document.querySelector('main').appendChild(h3);
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
+  fetchTotalBookPages();
 });
+
+/*
+Stretch deliverables:
+
+1. The 5th book in the series:
+    fetch('https://anapioficeandfire.com/api/books/5')
+
+2. The 1031st character in the series:
+    fetch('https://anapioficeandfire.com/api/characters/1031')
+
+3. The total number of pages of all the books:
+    fetchTotalBookPages() function above.  
+
+*/
+
+      // function computeTotalPages(books) {
+      //   let pageCount;
+      //   for (const book in books) {pageCount += books.numberOfPages};
+      //   //   for (const bookProperty in book)
+      //   //     if (bookProperty === "numberOfPages") {pageCount += book[bookProperty]}
+      //   // // }
+      //   debugger;
+      //   console.log(pageCount);
+      // }
